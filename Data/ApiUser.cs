fee0871e4b54af1f007a2ce6d8fd8eab25ad0a60
whyme0 <sparkjetstudiost@gmail.com>
2022-07-10 22:10:35 +0300

    create api for user

---
+++ Data/ApiUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.Data
+{
+    public class ApiUser : IdentityUser
+    {
+        public DateTime RegistrationDate { get; init; }
+        public ApiUser() : base()
+        {
+            RegistrationDate = DateTime.Now;
+        }
+    }
+}
