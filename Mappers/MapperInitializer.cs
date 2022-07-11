61388dc39750936e9b1f7eded512e5e1b9c6127d
whyme0 <sparkjetstudiost@gmail.com>
2022-07-11 18:45:14 +0300

    sets mapping between apiuser and apiuserdto models

--- Mappers/MapperInitializer.cs
+++ Mappers/MapperInitializer.cs
@@ -12,6 +12,7 @@ namespace HotelListing.Mappers
             CreateMap<Country, CreateCountryDTO>().ReverseMap();
             CreateMap<Hotel, HotelDTO>().ReverseMap();
             CreateMap<Hotel, CreateHotelDTO>().ReverseMap();
+            CreateMap<ApiUser, ApiUserDTO>().ReverseMap();
         }
     }
 }
