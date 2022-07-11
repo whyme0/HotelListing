6e92702dd258011fc6b9f4336fb540b70b9786a8
whyme0 <sparkjetstudiost@gmail.com>
2022-07-11 18:44:14 +0300

    create dto object for user model

---
+++ Models/ApiUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class ApiUserDTO
+    {
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 16, MinimumLength = 8, ErrorMessage = "Password length cannot be less than 8 characters or more than 16 characters!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
