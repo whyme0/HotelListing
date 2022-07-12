e3d12aea3d6a482d9b06347e8b428fa49956cae5
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 14:50:49 +0300

    created configuration for roles

--- Configurations/Data/RolesConfiguration.cs
+++ Configurations/Data/RolesConfiguration.cs
@@ -1,6 +1,30 @@
-﻿namespace HotelListing.Configurations.Data
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Configurations.Data
 {
-    public class RolesConfiguration
+    public class RolesConfiguration : IEntityTypeConfiguration<IdentityRole>
     {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole 
+                { 
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+                new IdentityRole()
+                {
+                    Name = "Moderator",
+                    NormalizedName = "MODERATOR"
+                },
+                new IdentityRole()
+                {
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                }
+            );
+        }
     }
 }
