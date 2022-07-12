3a1016aa15d4df387217d91d5c8fa3d311598746
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 14:34:22 +0300

    replace hotels configuration from databasecontext to another class

---
+++ Configurations/Data/HotelsConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Configurations.Data
+{
+    public class HotelsConfiguration : IEntityTypeConfiguration<Hotel>
+    {
+        public void Configure(EntityTypeBuilder<Hotel> builder)
+        {
+            builder.HasData(
+                new Hotel()
+                {
+                    Id = 1,
+                    Name = "Helvetia Hotel",
+                    Address = "Marata St., 11, St. Petersburg",
+                    Rating = 9.6,
+                    CountryId = 1
+                },
+                new Hotel()
+                {
+                    Id = 2,
+                    Name = "Hilton Kyiv",
+                    Address = "30 Tarasa Shevchenka Blvd",
+                    Rating = 8.5,
+                    CountryId = 2,
+                },
+                new Hotel()
+                {
+                    Id = 3,
+                    Name = "BonHotel",
+                    Address = "2 Pritytskogo St",
+                    Rating = 8.8,
+                    CountryId = 3
+                }
+            );
+        }
+    }
+}
