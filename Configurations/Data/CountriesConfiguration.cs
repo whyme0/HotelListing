3567a925e6641ed93c7aee7343943a667623ec56
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 14:40:29 +0300

    replace country configuration from databasecontext to another class

---
+++ Configurations/Data/CountriesConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Configurations.Data
+{
+    public class CountriesConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasData(
+                new Country()
+                {
+                    Id = 1,
+                    Name = "Russia",
+                    ShortName = "RUS"
+                },
+                new Country()
+                {
+                    Id = 2,
+                    Name = "Ukraine",
+                    ShortName = "UKR",
+                },
+                new Country()
+                {
+                    Id = 3,
+                    Name = "Belarus",
+                    ShortName = "BLR"
+                }
+            );
+        }
+    }
+}
