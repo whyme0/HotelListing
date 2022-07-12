c1882e340e7b46cbbaa1f90a5bac3a797ce7172e
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 14:48:44 +0300

    fix register action

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -24,7 +24,11 @@ namespace HotelListing.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody] ApiUserDTO apiUser)
+        [Route("signup/")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> SignUp([FromBody] RegistrationApiUserDTO apiUser)
         {
             _logger.LogInformation($"Registration attempt with email {apiUser.Email}");
             if (!ModelState.IsValid)
@@ -34,13 +38,45 @@ namespace HotelListing.Controllers
             try
             {
                 var user = _mapper.Map<ApiUser>(apiUser);
-                var result = await _userManager.CreateAsync(user);
+                user.UserName = user.Email.Split("@")[0];
+                var result = await _userManager.CreateAsync(user, apiUser.Password);
                 if (!result.Succeeded)
                 {
-                    return StatusCode(StatusCodes.Status400BadRequest, "User registration attempt failed");
+                    return StatusCode(StatusCodes.Status400BadRequest, result.Errors);
                 }
+                await _userManager.AddToRoleAsync(user, "User");
                 return StatusCode(StatusCodes.Status201Created, $"User with email {apiUser.Email} created");
 
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while proceeding request");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error. Please try again later.");
+            }
+        }
+
+
+        [HttpPost]
+        [Route("signin/")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> SignIn([FromBody] LoginApiUserDTO apiUser)
+        {
+            _logger.LogInformation($"Login attempt with email {apiUser.Email}");
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ModelState);
+            }
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(apiUser.Email, apiUser.Password, false, false);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, "User login attempt failed");
+                }
+                return StatusCode(StatusCodes.Status202Accepted, "Welcome!");
             }
             catch (Exception ex)
             {
