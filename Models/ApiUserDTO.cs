c4ba8de3c88da16ee7f3229d0f1a3b77a54ea610
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 12:31:51 +0300

    create apiuserdto for login

--- Models/ApiUserDTO.cs
+++ Models/ApiUserDTO.cs
@@ -2,6 +2,17 @@
 
 namespace HotelListing.Models
 {
+    public class LoginApiUserDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+
     public class ApiUserDTO
     {
         [DataType(DataType.PhoneNumber)]
