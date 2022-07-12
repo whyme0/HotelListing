eac52c8ab3c1fb0e00788069880a6873dbf6a42c
whyme0 <sparkjetstudiost@gmail.com>
2022-07-12 14:42:34 +0300

    databasecontext refactored. now it more clean

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HotelListing.Configurations.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,54 +28,10 @@ namespace HotelListing.Data
             builder.Entity<IdentityUserToken<string>>(e => e.ToTable("UserTokens"));
             builder.Entity<IdentityRoleClaim<string>>(e => e.ToTable("RoleClaims"));
 
-            // Seeds
-            builder.Entity<Country>().HasData(
-                new Country()
-                {
-                    Id = 1,
-                    Name = "Russia",
-                    ShortName = "RUS"
-                },
-                new Country()
-                {
-                    Id = 2,
-                    Name = "Ukraine",
-                    ShortName = "UKR",
-                },
-                new Country()
-                {
-                    Id = 3,
-                    Name = "Belarus",
-                    ShortName = "BLR"
-                }
-            );
-
-            builder.Entity<Hotel>().HasData(
-                new Hotel()
-                {
-                    Id = 1,
-                    Name = "Helvetia Hotel",
-                    Address = "Marata St., 11, St. Petersburg",
-                    Rating = 9.6,
-                    CountryId = 1
-                },
-                new Hotel()
-                {
-                    Id = 2,
-                    Name = "Hilton Kyiv",
-                    Address = "30 Tarasa Shevchenka Blvd",
-                    Rating = 8.5,
-                    CountryId = 2,
-                },
-                new Hotel()
-                {
-                    Id = 3,
-                    Name = "BonHotel",
-                    Address = "2 Pritytskogo St",
-                    Rating = 8.8,
-                    CountryId = 3
-                }
-            );
+            // Specific configurations for different entities separated in different classes
+            builder.ApplyConfiguration(new RolesConfiguration());
+            builder.ApplyConfiguration(new HotelsConfiguration());
+            builder.ApplyConfiguration(new CountriesConfiguration());
         }
     }
 }
